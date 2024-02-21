4f5535a7a905e0bb4d941b658b7ab51274a1871c
Yonatan Karp-Rudin <yonvata@gmail.com>
2024-02-21 07:46:48 +0100

    Extract Switch fixture from SwitchService test
    This commit extracts the switch fixtures functions into the SwitchFixture class

--- domain/src/testFixtures/kotlin/com/yonatankarp/topologyinventory/domain/entity/SwitchFixture.kt
+++ domain/src/testFixtures/kotlin/com/yonatankarp/topologyinventory/domain/entity/SwitchFixture.kt
@@ -1,6 +1,7 @@
 package com.yonatankarp.topologyinventory.domain.entity
 
 import com.yonatankarp.topologyinventory.domain.valueobject.IP
+import com.yonatankarp.topologyinventory.domain.valueobject.IPFixture
 import com.yonatankarp.topologyinventory.domain.valueobject.Id
 import com.yonatankarp.topologyinventory.domain.valueobject.Location
 import com.yonatankarp.topologyinventory.domain.valueobject.LocationFixture
@@ -41,4 +42,62 @@ object SwitchFixture {
         SwitchType.LAYER3,
         networks.toMutableList(),
     )
+
+    val switches: List<Switch>
+        get() =
+            listOf(
+                createSwitch(
+                    IPFixture.IPV4,
+                    12u,
+                    LocationFixture.createLocation("US"),
+                ),
+                createSwitch(
+                    IPFixture.IPV4,
+                    12u,
+                    LocationFixture.createLocation("GB"),
+                ),
+                createSwitch(
+                    IPFixture.IPV6,
+                    12u,
+                    LocationFixture.createLocation("DE"),
+                ),
+            )
+
+    val ipv4SwitchOnly: List<Switch>
+        get() =
+            listOf(
+                createSwitch(
+                    IPFixture.IPV4,
+                    12u,
+                    LocationFixture.createLocation("US"),
+                ),
+                createSwitch(
+                    IPFixture.IPV4,
+                    12u,
+                    LocationFixture.createLocation("GB"),
+                ),
+            )
+
+    val switchesMap: Map<Id, Switch>
+        get() =
+            mapOf(
+                Id.withId("82e24dc0-170b-45e0-b70f-9f133f0a33db") to
+                    createSwitch(
+                        IPFixture.IPV4,
+                        12u,
+                        LocationFixture.createLocation("US"),
+                    ),
+                Id.withId("176f5148-54fc-4646-9ac2-0e9d99d7c6e8") to
+                    createSwitch(
+                        IPFixture.IPV4,
+                        12u,
+                        LocationFixture.createLocation("GB"),
+                    ),
+                Id.withId("78c3ed36-4ca7-4adf-a4c6-97e6ce280e7a") to
+                    createSwitch(
+                        IPFixture.IPV6,
+                        12u,
+                        LocationFixture.createLocation("DE"),
+                    ),
+            )
 }

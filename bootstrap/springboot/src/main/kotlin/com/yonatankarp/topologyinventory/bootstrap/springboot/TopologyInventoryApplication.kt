3202fbb2f2f9a8137bcfa83556c29c2c803e8a56
Yonatan Karp-Rudin <yonvata@gmail.com>
2024-03-01 13:14:24 +0100

    Create modules for SpringBoot and Quarkus

--- bootstrap/springboot/src/main/kotlin/com/yonatankarp/topologyinventory/bootstrap/springboot/TopologyInventoryApplication.kt
+++ bootstrap/springboot/src/main/kotlin/com/yonatankarp/topologyinventory/bootstrap/springboot/TopologyInventoryApplication.kt
@@ -1,4 +1,4 @@
-package com.yonatankarp.topologyinventory.bootstrap
+package com.yonatankarp.topologyinventory.bootstrap.springboot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
